82800724f1b6c39d606c05e98dd28f596a4fbad4
xenomech <gokulsmenon227@gmail.com>
2021-04-20 20:45:03 +0530

    ➕ Google font plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,15 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `source sans pro\:300,400,400i,700`, 
+        ],
+        display: "swap",
+      },
+    },
   ],
 }

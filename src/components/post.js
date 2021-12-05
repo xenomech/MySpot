c95711d22a8d5038401dcab5ebf757dabefd6bb4
xenomech <gokulsmenon227@gmail.com>
2021-12-05 20:28:17 +0530

    💥 Migration to NextJS

--- src/components/post.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { PostWrapper } from "../styles"
-
-export const Post = ({ children }) => {
-  return <PostWrapper>{children}</PostWrapper>
-}
--- src/components/table.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { TableWrapper } from "../styles"
-export const Table = ({ children }) => {
-  return (
-    <TableWrapper>
-      <table>{children}</table>
-    </TableWrapper>
-  )
-}
